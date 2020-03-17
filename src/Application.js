f95fa0f4e5f7cc19cbdfebd2d019f62ca05f6a17
nirodhamwickramaratne@gmail.com <nirodhamwickramaratne@gmail.com>
2020-03-17 17:51:09 +0530

    initial commit

---
+++ src/Application.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+class Application extends Component{
+    render()
+    {
+    let name="Nirodha";
+    return (
+        <div>
+            <h1>Helo {name}</h1>
+            <span>This is my first react application</span>
+        </div>
+    );
+    }
+} 
+
+export default Application; 
